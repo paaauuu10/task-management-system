08165f55a92cc6acba80a8def55d61fecc0b4367
Pau Botargues Julio <pbotargu@student.42barcelona.com>
2025-09-09 20:01:29 +0200

    User model and integration with Django admin

--- django_backend/config/settings.py
+++ django_backend/config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "rest_framework",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#User
+AUTH_USER_MODEL = "users.User"
